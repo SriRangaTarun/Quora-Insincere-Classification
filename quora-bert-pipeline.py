a46da2fefbe632e7cbef3aea9f4f45099ff92848
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2020-05-17 14:23:59 +0530

    Update quora-bert-pipeline.py

--- quora-bert-pipeline.py
+++ quora-bert-pipeline.py
@@ -1,3 +1,5 @@
+# Import necessary libraries
+
 import os
 import gc
 import numpy as np
@@ -19,7 +21,23 @@ import transformers
 from torchsample.modules import ModuleTrainer
 from transformers import BertForSequenceClassification, BertTokenizer
 
-train_data_df = pd.read_csv('train.csv')
+# Define hyperparameters and paths
+
+parser = argparse.ArgumentParser()
+parser.add_argument('train_data_path')
+args = parser.parse_args(); path = args.train_data_path
+
+NUM_EPOCHS = 2
+BATCH_SIZE = 32
+LEARNING_RATE = 0.0001
+
+TRAIN_VAL_SPLIT = 0.8
+NUM_TEXT_FEATURES = 768
+MAX_SEQUENCE_LENGTH = 128
+
+# Load training data
+
+train_data_df = pd.read_csv(path)
 train_data = train_data_df.values
 
 targets = []
@@ -28,21 +46,19 @@ sentences = []
 for i in range(0, len(train_data)):
     targets.append(train_data[i][2])
     sentences.append(train_data[i][1])
-    
+
 targets = np.array(targets)
+
+# Define tokenizer
+
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-sequences = []
-bag_of_words = []
+# Get sequences and split train/val
 
+sequences = []
 for i in range(len(sentences)):
     tokenized_sentence = tokenizer.tokenize(sentences[i])
-    bag_of_words.append(tokenized_sentence)
     sequences.append(tokenizer.convert_tokens_to_ids(tokenized_sentence))
-
-TRAIN_VAL_SPLIT = 0.8
-NUM_TEXT_FEATURES = 768
-MAX_SEQUENCE_LENGTH = 128
 
 targets = FloatTensor(targets.reshape(len(targets), 1))
 sequences = LongTensor(sequence.pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH))
@@ -51,6 +67,8 @@ val_targets = targets[np.int32(TRAIN_VAL_SPLIT*len(targets)):]
 train_targets = targets[:np.int32(TRAIN_VAL_SPLIT*len(targets))]
 val_sequences = sequences[np.int32(TRAIN_VAL_SPLIT*len(sequences)):]
 train_sequences = sequences[:np.int32(TRAIN_VAL_SPLIT*len(sequences))]
+
+# Get pretrained BERT model
 
 NUM_TRANSFORMER_BLOCKS = 12
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased').cuda()
@@ -70,21 +88,18 @@ for param in model.bert.pooler.parameters():
 for param in model.bert.encoder.layer[NUM_TRANSFORMER_BLOCKS - 1].parameters():
   param.requires_grad = True
 
+# Add custom double-dense head to the BERT model
+
 model.classifier = Sequential(Linear(in_features=NUM_TEXT_FEATURES, out_features=100),
                               ReLU(), Linear(in_features=100, out_features=20, bias=True),
                               ReLU(), Linear(in_features=20, out_features=1, bias=True), Sigmoid()).cuda()
 
+# Set CuDNN benchmark to speed up training and create Torchsample trainer
+
 cudnn.benchmark = True
 trainer = ModuleTrainer(model)
 
-NUM_EPOCHS = 2
-BATCH_SIZE = 32
-LEARNING_RATE = 0.0001
-
-# GOOD BATCH SIZE + LEARNING RATE COMINATIONS :
-# 1) 32 AND 0.00001
-
-# Try decreasing the MAX SEQ LENGTH to 64 and increasing the BATCH SIZE to 64
+# Compile and train Torchsample trainer
 
 trainer.compile(loss='binary_cross_entropy',
                 optimizer=Adam(params=model.parameters(), lr=LEARNING_RATE))
