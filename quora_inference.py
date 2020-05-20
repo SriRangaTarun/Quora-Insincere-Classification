2df2e6be973396685d9a75b6358e735c262c8904
Tarun Paparaju <tarunp@greenwoodhigh.edu.in>
2020-05-20 22:35:24 +0530

    Update quora_inference.py

--- quora_inference.py
+++ quora_inference.py
@@ -11,14 +11,8 @@ from keras.preprocessing.sequence import pad_sequences as pad
 
 # Define hyperparameters and paths
 
-EPOCHS = 2
-SPLIT = 0.8
+MAXLEN = 64
 DROP_RATE = 0.3
-MAXLEN = 64 # 70
-LR = (4e-5, 1e-2)
-BATCH_SIZE = 256
-VAL_BATCH_SIZE = 8192
-MODEL_SAVE_PATH = 'insincerity_model.pt'
 
 # Load trained model for inference from path
 
@@ -28,47 +22,48 @@ tokenizer = RobertaTokenizer.from_pretrained(model)
 class Roberta(nn.Module):
     def __init__(self):
         super(Roberta, self).__init__()
-        self.softmax = nn.Softmax(dim=1)
-        self.drop = nn.Dropout(DROP_RATE)
+        self.dropout = nn.Dropout(DROP_RATE)
+        self.dense_output = nn.Linear(768, 1)
         self.roberta = RobertaModel.from_pretrained(model)
-        self.dense = nn.Linear(ROBERTA_UNITS, OUTPUT_UNITS)
-        
+
     def forward(self, inp, att):
         inp = inp.view(-1, MAXLEN)
         _, self.feat = self.roberta(inp, att)
-        return self.softmax(self.dense(self.drop(self.feat)))
+        return self.dense_output(self.dropout(self.feat))
     
 network = Roberta()
 parser = argparse.ArgumentParser()
 
 parser.add_argument('train_model_path')
 args = parser.parse_args(); path = args.train_model_path
-network.load_state_dict(torch.load(path + 'sentiment_model.pt'))
+network.load_state_dict(torch.load(path + 'insincerity_model.pt'))
 
 # Move model to GPU and set it to evaluation mode
 
 network = network.cuda().eval()
 
-# Create inference function to predict sentiment
+# Create inference function to predict insincerity
 
-def predict_sentiment(tweet):
+def predict_insincerity(question):
     pg, tg = 'post', 'post'
-    tweet_ids = tokenizer.encode(tweet.strip())
-    sent = {0: 'positive', 1: 'neutral', 2: 'negative'}
+    ins = {0: 'sincere', 1: 'insincere'}
+    quest_ids = tokenizer.encode(question.strip())
 
-    att_mask_idx = len(tweet_ids) - 1
-    if 0 not in tweet_ids: tweet_ids = 0 + tweet_ids
-    tweet_ids = pad([tweet_ids], maxlen=MAXLEN, value=1, padding=pg, truncating=tg)
+    attention_mask_idx = len(quest_ids) - 1
+    if 0 not in quest_ids: quest_ids = 0 + quest_ids
+    quest_ids = pad([quest_ids], maxlen=MAXLEN, value=1, padding=pg, truncating=tg)
 
     att_mask = np.zeros(MAXLEN)
-    att_mask[1:att_mask_idx] = 1
+    att_mask[1:attention_mask_idx] = 1
     att_mask = att_mask.reshape((1, -1))
-    if 2 not in tweet_ids: tweet_ids[-1], att_mask[-1] = 2, 0
-    tweet_ids, att_mask = LongTensor(tweet_ids).cuda(), LongTensor(att_mask).cuda()
-    return sent[np.argmax(network.forward(tweet_ids, att_mask).detach().cpu().numpy())]
+    if 2 not in quest_ids: quest_ids[-1], attention_mask[-1] = 2, 0
+    quest_ids, att_mask = torch.LongTensor(quest_ids), torch.LongTensor(att_mask)
+    
+    output = network.forward(quest_ids.cuda(), att_mask.cuda())
+    return ins[int(np.round(nn.Sigmoid()(output.detach().cpu()).item()))]
 
-# Predict sentiment on random tweet
+# Predict insincerity on random question
 
-parser.add_argument('text')
+parser.add_argument('quest')
 args = parser.parse_args()
-print(predict_sentiment(args.tweet)))
+print(predict_insincerity(args.quest))
